606ef80e80d81ca2f8850ec9755a6f4d0d0dd1c8
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-07-05 22:09:23 +0000

    first

---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='text-center bg-dark footer'>
+        <br/> <p>Copyright c 2023</p> <br/>
+    </div>
+  )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,27 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar";
+import Card from "./card";
+import Jumbotron from "./jumbotron";
+import Footer from "./footer";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div classNameName="text-center mx-auto w-100">
+			<Navbar/>
+			<Jumbotron/>
+			<div className="d-flex mx-auto justify-content-center my-3">
+				<Card img={"https://helios-i.mashable.com/imagery/articles/05Ym34W6tH3CyuWYOCbpCAr/hero-image.fill.size_1200x675.v1623391769.jpg"}/>
+				<Card img={"https://helios-i.mashable.com/imagery/articles/05Ym34W6tH3CyuWYOCbpCAr/hero-image.fill.size_1200x675.v1623391769.jpg"}/>
+				<Card img={"https://helios-i.mashable.com/imagery/articles/05Ym34W6tH3CyuWYOCbpCAr/hero-image.fill.size_1200x675.v1623391769.jpg"}/>
+				<Card img={"https://helios-i.mashable.com/imagery/articles/05Ym34W6tH3CyuWYOCbpCAr/hero-image.fill.size_1200x675.v1623391769.jpg"}/>
+				
+			</div>
+			<div>
+				<Footer/>
+			</div>
 		</div>
 	);
 };
